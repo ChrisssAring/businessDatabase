a7a11508fb44d5815eaee3a5f94bda1ac42ba8a4
Chris <Chris@DESKTOP-GARVSDS>
2015-11-23 15:25:45 -0500

    Deleted Bootstrap :(. Basic CSS started.

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ and open the template in the editor.
 -->
 <html lang="en">
     <head>
+    <link rel="stylesheet" type="text/css" href="style.css"> 
     <meta charset="utf-8" />
     </head>
     <body>
-        <form action="loginBusiness.php">
-            Business Login: <input type="submit" value="Go" />
+        <h2>Assist-Base</h2>
+        <div class="wrapper">
+            
+        <form action="loginBusiness.php" class="pure-form">
+            <button class="button-secondary "type="submit">Business Login</button>
+                      <button class="button-secondary "type="submit" formaction="loginUser.php">User Login</button><br>
         </form>
-        <form action="loginUser.php">
-            User Login: <input type="submit" value="Go" />
-        </form>
+    </div>
     </body>
 </html>
